45fbd96ef6cc31763a1055a035ecaf57e56a9a3d
the-chronicles <oluseungideon@gmail.com>
2023-10-31 04:20:15 +0100

    Added Questions and Options

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -31,13 +33,21 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error("Action Unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -58,7 +68,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]}  dispatch={dispatch} answer={answer} />}
       </Main>
     </div>
   );
---
+++ src/components/Options.js
@@ -0,0 +1,21 @@
+function Options({ question, dispatch, answer }) {
+  return (
+    <div>
+      <div className="options">
+        {question.options.map((option, index) => (
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""}${
+              index === question.correctOption ? "correct" : "wrong"
+            }`}
+            key={option}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Options;
