1cc35e0f352a011a03702c4bfa54c3b2dc4d8724
the-chronicles <oluseungideon@gmail.com>
2023-10-31 02:18:10 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <p>1</p>
+        <p>Question?</p>
+      </main>
+    </div>
+  );
+}
